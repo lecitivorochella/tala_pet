edaabd08c07a1a6e45a4b69661704c9c41f370bd
Clarence <clarencebueza24@gmail.com>
2025-05-05 16:48:59 +0800

    edited again

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -9,16 +9,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active text-white fw-bold" href="{{ route('petui') }}">Home</a>
+          <a class="nav-link active text-dark fw-bold" href="{{ route('petui') }}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white fw-bold" href="#">Our Pets</a>
+          <a class="nav-link text-dark fw-bold" href="#">Our Pets</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white fw-bold" href="#">Why Adopt</a>
+          <a class="nav-link text-dark fw-bold" href="#">Why Adopt</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white fw-bold" href="#">FAQs</a>
+          <a class="nav-link text-dark fw-bold" href="#">FAQs</a>
         </li>
       </ul>
 
